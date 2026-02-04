0230f0803824aec134da36ca66e81e4db826f889
Ratinan Lee <lee.ratinan@gmail.com>
2026-02-05 01:11:29 +0800

    cart api fe wip

--- app/Views/store_info_page.php
+++ app/Views/store_info_page.php
@@ -15,6 +15,9 @@
                 </div>
             </div>
             <?php if ('products' == $type) : ?>
+                <pre>
+                    <?php print_r($products); ?>
+                </pre>
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
@@ -38,7 +41,9 @@
                                             <div class="row">
                                                 <?php foreach ($products['variants'] as $variant) : ?>
                                                     <div class="col-6 col-md-12 col-lg-6 col-xl-4">
-                                                        <h3><?= $variant['variant_name'] ?></h3>
+                                                        <?php if (1 < count($products['variants'])) : ?>
+                                                            <h3><?= $variant['variant_name'] ?></h3>
+                                                        <?php endif; ?>
                                                         <?php if (!empty($variant['variant_sku'])) : ?>
                                                             <p class="small mb-0">SKU: <?= $variant['variant_sku'] ?></p>
                                                         <?php endif; ?>
@@ -51,10 +56,14 @@
                                                             </p>
                                                             <?php if (0 < $variant['inventory_count']) : ?>
                                                                 <div class="input-group mb-2">
-                                                                    <span class="input-group-text"><label for="quantity-<?= $variant['variant_slug'] ?>"><?= lang('System.store.quantity') ?></label></span>
-                                                                    <input type="number" class="form-control" id="quantity-<?= $variant['variant_slug'] ?>" name="quantity" value="1" min="1" max="<?= min(10, $variant['inventory_count']) ?>" />
+                                                                    <span class="input-group-text"><label for="quantity-<?= $products['id'] ?>-<?= $variant['id'] ?>"><?= lang('System.store.quantity') ?></label></span>
+                                                                    <input type="number" class="form-control" id="quantity-<?= $products['id'] ?>-<?= $variant['id'] ?>" name="quantity" value="1" min="1" max="<?= min(10, $variant['inventory_count']) ?>" />
                                                                 </div>
-                                                                <a class="btn btn-dark w-100" data-variant-slug="<?= $variant['variant_slug'] ?>" href="<?= base_url($locale . '/@' . $business['business_slug'] . '/add-to-cart/' . $products['product_slug'] . '/' . $variant['variant_slug']) ?>"><?= lang('System.store.add-to-cart') ?></a>
+                                                                <button class="btn btn-dark w-100 btn-add-to-cart"
+                                                                        data-product-id="<?= $products['id'] ?>" data-variant-id="<?= $variant['id'] ?>"
+                                                                        data-product-name="<?= $products['product_name'] ?>" data-variant-name="<?= $variant['variant_name'] ?>"
+                                                                        data-price="<?= $variant['price_active'] ?>" data-product-type="<?= $products['product_type'] ?>"
+                                                                ><?= lang('System.store.add-to-cart') ?></button>
                                                             <?php else: ?>
                                                                 <p class="alert alert-danger py-2 px-3"><?= lang('System.store.out-of-stock') ?></p>
                                                             <?php endif; ?>
@@ -132,6 +141,44 @@
     </main>
     <script>
         document.addEventListener("DOMContentLoaded", function () {
+            <?php if ('products' == $type) : ?>
+            $('.btn-add-to-cart').click(function (e) {
+                e.preventDefault();
+                let product_id    = $(this).data('product-id'),
+                    variant_id    = $(this).data('variant-id'),
+                    quantity      = $('#quantity-'+product_id+'-'+variant_id).val(),
+                    product_name  = $(this).data('product-name'),
+                    variant_name  = $(this).data('variant-name'),
+                    price         = $(this).data('price'),
+                    product_type  = $(this).data('product-type'),
+                    line_subtotal = price * quantity;
+                $.post(
+                    "<?= base_url($locale . '/@' . $business['business_slug'] . '/add-to-cart') ?>",
+                    {
+                        product_variant_id: variant_id,
+                        product_name: product_name,
+                        product_variant_name: variant_name,
+                        line_quantity: quantity,
+                        unit_price: price,
+                        line_subtotal: line_subtotal
+                        item_need_delivery: product_type
+                    },
+                    function (response, status) {
+                        if (response.status === "OK") {
+                            toastr.success(response.message);
+                            setTimeout(function() { location.reload(); }, 3000);
+                        } else {
+                            toastr.error(response.message);
+                        }
+                    },
+                    "json"
+                ).fail(function (response) {
+                    let message = response.responseJSON.message ?? '<?= lang('System.response-msg.error.generic') ?>';
+                    toastr.error(message);
+                });
+            });
+            <?php else: ?>
+            <?php endif; ?>
             // $('.btn-tab').click(function (e) {
             //     e.preventDefault();
             //     $('.tab-content').slideUp();
